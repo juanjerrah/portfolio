7dd64d87d83ff8d3e313af66a2952bcb309d28cc
Juanjerrah <juan.oliveira@avonale.com.br>
2023-07-19 18:10:37 -0300

    Creating components

---
+++ src/components/index.ts
@@ -0,0 +1,21 @@
+import About from "./About";
+import Contact from "./Contact";
+import Experience from "./Experience";
+import Feedbacks from "./Feedbacks";
+import Hero from "./Hero";
+import Loader from "./Loader";
+import Navbar from "./Navbar";
+import Tech from "./Tech";
+import Works from "./Works";
+
+export {
+    Navbar,
+    Hero,
+    About,
+    Contact,
+    Experience,
+    Feedbacks,
+    Loader,
+    Tech,
+    Works
+}
